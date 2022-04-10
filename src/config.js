c85e212263ea0c5cd817a9d4af5d69447ff6732b
Carlos Gallardo <carlitos.gallardo009@gmail.com>
2022-04-10 17:56:12 -0500

    prod

--- src/config.js
+++ src/config.js
@@ -2,4 +2,5 @@ import { config } from "dotenv";
 config();
 export default {
   mongodbURL: process.env.DATABASE_URI || "mongodb://localhost/crud-socketio",
+  port: process.env.PORT || 5000,
 };
--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import app from "./app";
 import http from "http";
 import sockets from "./sockets";
 import "./database";
+import config from "./config";
 const server = http.createServer(app);
-const httpServer = server.listen(5000);
-console.log("server on port", 5000);
+const httpServer = server.listen(config.port);
+console.log("server on port", config.port);
 const io = new WebSocketServer(httpServer);
 
 sockets(io);
